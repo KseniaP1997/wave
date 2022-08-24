3dda873752f8e846fe9038a0218ab892d3d0c00a
KseniaP1997 <110461473+KseniaP1997@users.noreply.github.com>
2022-08-24 13:24:49 +0300

    wave

--- js/script.js
+++ js/script.js
@@ -37,7 +37,7 @@ document.querySelectorAll('.tabs-nav__btn').forEach(function(tabsBtn){
   tabsBtn.addEventListener('click', function(e){
     const path = e.currentTarget.dataset.path;
     document.querySelectorAll('.tabs-nav__btn').forEach(function(btn){
-btn.classList.remove('tabs-nav__btn--active')});
+    btn.classList.remove('tabs-nav__btn--active')});
     e.currentTarget.classList.add('tabs-nav__btn--active');
 document.querySelectorAll('.tabs-item').forEach(function(tabsBtn){
 tabsBtn.classList.remove('tabs-item--active')});
